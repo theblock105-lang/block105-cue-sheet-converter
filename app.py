f0f19a56932b22fa73e00c6465f68454abce7f32
theblock105-lang <theblock105@yahoo.com>
2026-09-19 19:34:43 -0700

    Fix template syntax

--- app.py
+++ app.py
@@ -880,12 +880,12 @@ def index():
         )
 
         return render_template(
-           "converter.html"
+          "converter.html",
             report=report
         )
 
     return render_template(
-       "converter.html"
+     "converter.html",
         report=None
     )
 
